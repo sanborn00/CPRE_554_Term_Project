c7164b31c3eadb6f953f13f95c8cbfa698cd586c
Chung0754 <Zhongmohan@gmail.com>
2021-10-30 11:51:52 -0500

    clear cashed and update files

---
+++ Client/Client/ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by Shuangquan on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
