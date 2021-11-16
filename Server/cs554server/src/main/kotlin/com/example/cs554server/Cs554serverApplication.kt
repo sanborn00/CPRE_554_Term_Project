db243628f4f9bc7dd805f437081326953ae61993
Chung0754 <Zhongmohan@gmail.com>
2021-11-16 06:45:31 -0600

    [feature] Add two api on server 1. return a sample layout json 2. echo sending post request payload

--- Server/cs554server/src/main/kotlin/com/example/cs554server/Cs554serverApplication.kt
+++ Server/cs554server/src/main/kotlin/com/example/cs554server/Cs554serverApplication.kt
@@ -1,15 +1,14 @@
 package com.example.cs554server
 
-import io.netty.util.internal.ThreadLocalRandom
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
-import java.time.Duration
-import java.time.LocalDateTime
+
 
 @SpringBootApplication
 class Cs554serverApplication
@@ -19,19 +18,29 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class RestController() {
-	@GetMapping(value = ["/stocks/{symbol}"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-	fun prices(@PathVariable symbol: String): Flux<StockPrice> {
-		return Flux.interval(Duration.ofSeconds(1))
-				.map { StockPrice(symbol, randomStockPrice(), LocalDateTime.now())
+class LayoutTest01Resource {
+	@GetMapping(value = ["/LayoutTest01"])
+	fun sampleLayout(): Layout {
+		var layout = Layout(1, listOf(
+			Component(4, "This is a text field", ""),
+			Component(1, "This is a button", "nil"),
+			Component(2, "This is a list", ""),
+		))
+		return layout
+	}
+
+data class Message(val id: String?, val text: String)
+data class Component(val variant: Int, val data: String?, val action: String?)
+data class Layout(val pageID: Int, val components: List<Component>)
+
+
+}
+
+@RestController
+class EchoPayloadResource {
+	@PostMapping(value = ["/EchoPayload"])
+	fun echo(@RequestBody payload: String): String{
+		return payload
 	}
 }
 
-data class StockPrice(val symbol: String,
-					  val price: Double,
-					  val time: LocalDateTime)
-
-	private fun randomStockPrice(): Double {
-		return ThreadLocalRandom.current().nextDouble(100.0)
-	}
-}
