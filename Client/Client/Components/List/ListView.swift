bdfc0308f9a4a72bdb297b76d629358b3d500c0b
xiao liang <junior@xiaos-Air.lan>
2021-12-13 00:10:22 -0600

    Implement list

---
+++ Client/Client/Components/List/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Client
+//
+//  Created by xiao liang on 12/12/21.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
---
+++ Client/Client/Components/List/ListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ListViewModel.swift
+//  Client
+//
+//  Created by xiao liang on 12/12/21.
+//
+
+import SwiftUI
+
+struct ListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ListViewModel()
+    }
+}
